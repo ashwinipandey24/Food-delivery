9f8b3725c0a092c7845afb4acb8b77d555b672c5
ashwinipandey24 <pandeyashwini54@gmail.com>
2025-08-03 00:08:29 +0530

    project-done

---
+++ frontend/src/components/Navbar/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+      <h2>Order your favourite food here</h2>
+      <p>Choose from a diverse menu featuring a delecatable array of dishes crafted with finest ingrident and culinery expertise.Our mission is to satisfy your craving and elevateyour dining experience.One delicious meal at a time.</p>
+    <button>View Menu</button>
+    </div>
+    </div>
+  )
+}
+
+export default Header
